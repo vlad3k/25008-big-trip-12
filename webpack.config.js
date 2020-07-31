0d80f7925d2f4ac96bd5a3e304b5d7c0dc8792f7
vlad3k <vlad3k@bk.ru>
2020-07-31 21:42:51 +0300

    feature: set webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,9 @@ module.exports = {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public'),
   },
-  devtool: 'sourcemap'
+  devtool: 'sourcemap',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true
+  }
 }
